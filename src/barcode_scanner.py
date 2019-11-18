d4b74c67dae3d9d8df8e3f301ecd784a6fb8a403
alvarovillena <avillena@robotnik.es>
2019-11-18 13:26:42 +0100

    Update with serial version of barcode reader node

--- src/barcode_scanner.py
+++ src/barcode_scanner.py
@@ -1,109 +1,181 @@
 #!/usr/bin/env python
-# coding=utf-8
+
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2015, Robotnik Automation SLL
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of Robotnik Automation SSL nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import sys
 
 import rospy
-import evdev
-import datetime
-from evdev import InputDevice, categorize, ecodes
-from std_msgs.msg import Header
 
-'''
-  See where is the device "BarCode WPM ..."
-  $ lsusb
-  - Find BUS 00x Device 0xx: ID idVendor:idProduct Winbond Electronics Corp.
-  - Create an udev rule
-  $ sudo nano /etc/udev/rules.d/40-scanner.rules
-  - And write (modifying idVendor and idProduct if is necessary):
-  KERNEL=="event[0-9]*", SUBSYSTEM=="input", ATTRS{idVendor}=="0416", ATTRS{idProduct}=="c141", SYMLINK="input/barcode_scanner",GROUP="dialout", MODE="0666"
-  - Execute:
-  $ sudo service udev restart
-  $ sudo udevadm trigger
-  - Check if the link has been created:
-  $ ls -ln /dev/input | grep barcode
+import time, threading
 
-'''
+from rcomponent import RComponent, DEFAULT_FREQ, MAX_FREQ # esto podria estar como limites
 
-keys = {
-    # Scancode: ASCIICode
-    0: None, 1: u'ESC', 2: u'1', 3: u'2', 4: u'3', 5: u'4', 6: u'5', 7: u'6', 8: u'7', 9: u'8',
-    10: u'9', 11: u'0', 12: u'-', 13: u'=', 14: u'BKSP', 15: u'TAB', 16: u'Q', 17: u'W', 18: u'E', 19: u'R',
-    20: u'T', 21: u'Y', 22: u'U', 23: u'I', 24: u'O', 25: u'P', 26: u'[', 27: u']', 28: u'CRLF', 29: u'LCTRL',
-    30: u'A', 31: u'S', 32: u'D', 33: u'F', 34: u'G', 35: u'H', 36: u'J', 37: u'K', 38: u'L', 39: u';',
-    40: u'"', 41: u'`', 42: u'LSHFT', 43: u'\\', 44: u'Z', 45: u'X', 46: u'C', 47: u'V', 48: u'B', 49: u'N',
-    50: u'M', 51: u',', 52: u'.', 53: u'/', 54: u'RSHFT', 56: u'LALT', 100: u'RALT'
-}
+import robotnik_msgs.msg
 
-class BarCodeScanner:
+import serial
 
-  def __init__(self, args):
-    self.topic = args['topic']
-    self.port = args['port']
-    self.dev = InputDevice(self.port)
-    self.seq = 0
-    self.init_time = datetime.datetime.now()
-    rospy.loginfo(self.dev)
+import std_msgs.msg
 
-    # Create publisher
-    self.pub = rospy.Publisher(self.topic, Header, queue_size=10)
+class BarcodeScannerComponent(RComponent):
+    def __init__(self, args):
+        RComponent.__init__(self, args)
 
-  def scanBarcode(self):
-    barcode = ''
-    while True:
-        event = self.dev.read_one()
-        if event is None and barcode == '':
-          #There are blank events in between characters, 
-          #so we don't want to break if we've started
-          #reading them
-          if rospy.is_shutdown():
-					  exit()
-          break #nothing of importance, start a new read. 
+        self.port = args['port']
+        self.topic = args['topic']
+        self.read_errors = 0
+        self.serial_device = None
+        self.barcode_publisher_ = None
+
+    def setup(self):
+        if self.initialized:
+            rospy.logwarn("%s::setup: already initialized" % self.node_name)
+            return 0
+
+        RComponent.setup(self)
+
+        rospy.loginfo("%s::setup" % self.node_name)
+
+        self.serial_device = serial.Serial(
+            port=self.port,
+            baudrate=9600,
+            parity=serial.PARITY_NONE,
+            stopbits=1,
+            bytesize=8,
+            xonxoff=False,
+            dsrdtr=False,
+            rtscts=False
+        )
+
+
+    def shutdown(self):
+        if self.running:
+            rospy.logwarn("%st::shutdown: cannot shutdown because the component is still running" % self.node_name)
+            return -1
+        elif not self.initialized:
+            rospy.logwarn("%s::shutdown: cannot shutdown because the component was not setup" % self.node_name)
+            return -1
+
+        RComponent.shutdown(self)
+
+        rospy.loginfo("barcode::shutdown")
+
+        if self.serial_device != None and not self.serial_device.closed:
+            self.serial_device.close()
+
+        return 0
+
+    def rosSetup(self):
+        if self.ros_initialized:
+            rospy.logwarn("%s::rosSetup: already initialized" % self.node_name)
+            return 0
+        RComponent.rosSetup(self)
+
+        self.barcode_publisher_ = rospy.Publisher(self.topic, std_msgs.msg.Header, queue_size=1)
+
+    def rosShutdown(self):
+        if self.running:
+            rospy.logwarn("%s::rosShutdown: cannot shutdown because the component is still running" % self.node_name)
+            return -1
+        elif not self.ros_initialized:
+            rospy.logwarn("%s::rosShutdown: cannot shutdown because the component was not setup" % self.node_name)
+            return -1
+
+        RComponent.rosShutdown(self)
+
+        #self.set_digital_outputs_service_.shutdown()
+        #self.get_sw_version_service_.shutdown()
+        self.barcode_publisher_.unregister()
+
+    def readyState(self):
+        # currently we only have codes of 7 characters (plus 1 of end of line)
+        size_of_code = 8
+        code = self.readFromSerialDevice(size_of_code) # 8 bytes
+
+        if size_of_code == len(code):
+            msg = std_msgs.msg.Header()
+            msg.frame_id = code[0:-1]
+            self.barcode_publisher_.publish(msg)
+        else:
+            self.read_errors += 1
+
+        max_read_errors = 20
+        if self.read_errors >= max_read_errors:
+            self.switchToState(State.FAILURE_STATE)
+
+    def rosPublish(self):
+        RComponent.rosPublish(self)
+
+    def writeToSerialDevice(self, data):
+        bytes_written = self.serial_device.write(data)
+        return bytes_written
+
+    def readFromSerialDevice(self, size):
         try:
-          if event is not None:
-            if event.type == ecodes.EV_KEY:
-              data = categorize(event)
-              if data.keystate == 0 and data.scancode != 42: # Catch only keyup, and not Enter
-                if data.scancode == 28: #looking return key to be pressed
-                  #rospy.loginfo(barcode)
-                  msg = Header()
-                  msg.seq = self.seq
-                  self.seq = self.seq + 1
-                  time_diff = datetime.datetime.now() - self.init_time
-                  msg.stamp.secs = time_diff.seconds
-                  msg.stamp.nsecs = time_diff.microseconds * 1000
-                  msg.frame_id = barcode
-                  self.pub.publish(msg)
-                  barcode = ''
-                else:
-                    barcode += keys[data.scancode] # add the new char to the barcode
-        except AttributeError:
-            rospy.logerr("error parsing stream")
-            return 'SOMETHING WENT WRONG'
+            data_read = self.serial_device.read(size)
+        except serial.SerialException:
+            rospy.logwarn("Serial exception: Shutting node down")
+            self.rosShutdown()
 
+        return data_read
+
+def main():
+    rospy.init_node("barcode")
+
+    _name = rospy.get_name().replace('/','')
+
+    arg_defaults = {
+      'topic_state': 'state',
+      'topic': 'barcode_scanner',
+      'desired_freq': DEFAULT_FREQ,
+      'port' : '/dev/ttyACM0',
+    }
+
+    args = {}
+
+    for name in arg_defaults:
+        try:
+            if rospy.search_param(name):
+                args[name] = rospy.get_param('~%s'%(name)) # Adding the name of the node, because the para has the namespace of the node
+            else:
+                args[name] = arg_defaults[name]
+            #print name
+        except rospy.ROSException, e:
+            rospy.logerr('%s: %s'%(e, _name))
+
+
+    barcode_node = BarcodeScannerComponent(args)
+
+    barcode_node.start()
 
 if __name__ == "__main__":
-  
-  rospy.init_node("barcode_scanner")
-
-  _name = rospy.get_name().replace('/','')
-
-  arg_defaults = {
-    'topic': 'barcode_scanner',
-    'port': '/dev/input/barcode_scanner'
-  }
-  
-  args = {}
-  
-  for name in arg_defaults:
-    try:
-      if rospy.search_param(name): 
-        args[name] = rospy.get_param('~%s'%(name)) # Adding the name of the node, because the param has the namespace of the node
-      else:
-        args[name] = arg_defaults[name]
-    except rospy.ROSException, e:
-      rospy.logerr('%s: %s'%(e, _name))
-  
-
-  rc_node = BarCodeScanner(args)
-
-  while(True):
-    rc_node.scanBarcode()
+    main()
